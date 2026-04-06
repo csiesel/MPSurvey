cf83cd329ddeaa85176fbc809f493bf01cf7d181
csiesel <siesel.casey@gmail.com>
2026-04-06 09:54:45 -0400

    Updating the analysis functions to allow for numeric variables in the output

--- R/analyze_survey.R
+++ R/analyze_survey.R
@@ -1,54 +1,77 @@
 #' Analyze Survey Data
 #'
-#' This function analyzes survey data using the `gtsummary` package. It creates a summary table of specified variables, stratified by sex, and includes overall statistics, sample sizes, and confidence intervals.
+#' This function analyzes survey data using the `gtsummary` package. It creates
+#' a summary table of specified variables, stratified by sex/gender, and includes
+#' overall statistics, sample sizes, and confidence intervals.
 #'
 #' @param mps A survey design object created using the `srvyr` package.
 #' @param vars A character vector of variable names to include in the analysis.
+#' @param numeric_vars An optional character vector of variable names from `vars`
+#'   that should be interpreted as continuous/numeric in `tbl_svysummary()`.
+#'   Default is NULL.
 #'
-#' @return A `gtsummary` object containing the summary table of the analyzed survey data.
+#' @return A `gtsummary` object containing the summary table of the analyzed
+#'   survey data.
 #' @export
-#'
-#' @examples
-#' \dontrun{
-#' # Example usage:
-#' summary_table <- analyze_survey(mps = your_survey_design, vars = c("var1", "var2", "var3"))
-#' }
-analyze_survey <- function(mps, vars){
-
+analyze_survey <- function(mps, vars, numeric_vars = NULL){
 
   mps$variables <- mps$variables %>%
-    dplyr::mutate(dplyr::across(dplyr::everything(), ~ifelse(. %in% "NOT APPLICABLE", NA, .)))
-
+    dplyr::mutate(
+      dplyr::across(
+        dplyr::everything(),
+        ~ ifelse(. %in% "NOT APPLICABLE", NA, .)
+      )
+    )
 
   gtsummary::theme_gtsummary_compact()
   list("style_number-arg:big.mark" = "") %>%
     gtsummary::set_gtsummary_theme()
 
+  if (is.null(numeric_vars)) {
+    numeric_vars <- character(0)
+  } else {
+    numeric_vars <- intersect(as.character(numeric_vars), as.character(vars))
+  }
+
+  if (length(numeric_vars) > 0) {
+    mps$variables <- mps$variables %>%
+      dplyr::mutate(
+        dplyr::across(
+          dplyr::all_of(numeric_vars),
+          ~ suppressWarnings(as.numeric(as.character(.)))
+        )
+      )
+  }
+
+  type_list <- NULL
+  if (length(numeric_vars) > 0) {
+    type_list <- stats::setNames(
+      rep("continuous", length(numeric_vars)),
+      numeric_vars
+    )
+    type_list <- as.list(type_list)
+  }
 
   if("sex" %in% names(mps$variables)){
     gtsummary_indicators <- mps %>%
-      # Selecting all indicators
-      select(dplyr::all_of(c(vars, names(mps$strata)))) %>%
-      # Similar tbl_svysummary as above with a few differences
-      gtsummary::tbl_svysummary(by = sex,
-                                digits = list(gtsummary::all_categorical() ~ c(0,0,1,1,1)),
-                                # Setting the varible type as continuous for the days indicators
-                                # type = list(fruit_days ~ "continuous",
-                                #             fruit_servings ~ "continuous",
-                                #             veg_days ~ "continuous",
-                                #             veg_servings ~ "continuous",
-                                #             fruit_veg_servings ~ "continuous",
-                                #             pa_days ~ "continuous"),
-                                missing = "no",
-                                statistic = list(
-                                  # Formatting the continuous variables to be presented as mean and standard deviation
-                                  gtsummary::all_continuous() ~ "{n_unweighted}/{N_unweighted}/n{mean} ({sd})",
-                                  gtsummary::all_categorical() ~ "{n_unweighted}/{N_unweighted}\n{p}%")) %>%
+      dplyr::select(dplyr::all_of(c(vars, names(mps$strata)))) %>%
+      gtsummary::tbl_svysummary(
+        by = sex,
+        digits = list(gtsummary::all_categorical() ~ c(0, 0, 1, 1, 1)),
+        type = type_list,
+        missing = "no",
+        statistic = list(
+          gtsummary::all_continuous() ~ "{N_nonmiss_unweighted}/{N_obs_unweighted}\n{mean} ({sd})",
+          gtsummary::all_categorical() ~ "{n_unweighted}/{N_unweighted}\n{p}%"
+        )
+      ) %>%
       gtsummary::add_overall() %>%
-      # gtsummary::add_n(statistic="{N_nonmiss_unweighted}", location="level") %>%
-      gtsummary::add_ci(style_fun = list(
-        gtsummary::all_continuous() ~ gtsummary::label_style_number(digits = 2),
-        gtsummary::all_categorical() ~ gtsummary::label_style_percent(digits=2))) %>%
+      gtsummary::add_ci(
+        style_fun = list(
+          gtsummary::all_continuous() ~ gtsummary::label_style_number(digits = 2),
+          gtsummary::all_categorical() ~ gtsummary::label_style_percent(digits = 2)
+        )
+      ) %>%
       gtsummary::modify_header(
         label = "**Characteristic**",
         stat_0 = "**Overall**",
@@ -56,32 +79,29 @@ analyze_survey <- function(mps, vars){
         stat_1 = "**Female**",
         ci_stat_1 = "**95% CI**",
         stat_2 = "**Male**",
-        ci_stat_2 = "**95% CI**")
-  }
-  else if("gender" %in% names(mps$variables)){
+        ci_stat_2 = "**95% CI**"
+      )
+
+  } else if("gender" %in% names(mps$variables)){
     gtsummary_indicators <- mps %>%
-      # Selecting all indicators
-      select(dplyr::all_of(c(vars, names(mps$strata)))) %>%
-      # Similar tbl_svysummary as above with a few differences
-      gtsummary::tbl_svysummary(by = gender,
-                                digits = list(gtsummary::all_categorical() ~ c(0,0,1,1,1)),
-                                # Setting the varible type as continuous for the days indicators
-                                # type = list(fruit_days ~ "continuous",
-                                #             fruit_servings ~ "continuous",
-                                #             veg_days ~ "continuous",
-                                #             veg_servings ~ "continuous",
-                                #             fruit_veg_servings ~ "continuous",
-                                #             pa_days ~ "continuous"),
-                                missing = "no",
-                                statistic = list(
-                                  # Formatting the continuous variables to be presented as mean and standard deviation
-                                  gtsummary::all_continuous() ~ "{n_unweighted}/{N_unweighted}/n{mean} ({sd})",
-                                  gtsummary::all_categorical() ~ "{n_unweighted}/{N_unweighted}\n{p}%")) %>%
+      dplyr::select(dplyr::all_of(c(vars, names(mps$strata)))) %>%
+      gtsummary::tbl_svysummary(
+        by = gender,
+        digits = list(gtsummary::all_categorical() ~ c(0, 0, 1, 1, 1)),
+        type = type_list,
+        missing = "no",
+        statistic = list(
+          gtsummary::all_continuous() ~ "{N_nonmiss_unweighted}/{N_obs_unweighted}\n{mean} ({sd})",
+          gtsummary::all_categorical() ~ "{n_unweighted}/{N_unweighted}\n{p}%"
+        )
+      ) %>%
       gtsummary::add_overall() %>%
-      # gtsummary::add_n(statistic="{N_nonmiss_unweighted}", location="level") %>%
-      gtsummary::add_ci(style_fun = list(
-        gtsummary::all_continuous() ~ gtsummary::label_style_number(digits = 2),
-        gtsummary::all_categorical() ~ gtsummary::label_style_percent(digits=2))) %>%
+      gtsummary::add_ci(
+        style_fun = list(
+          gtsummary::all_continuous() ~ gtsummary::label_style_number(digits = 2),
+          gtsummary::all_categorical() ~ gtsummary::label_style_percent(digits = 2)
+        )
+      ) %>%
       gtsummary::modify_header(
         label = "**Characteristic**",
         stat_0 = "**Overall**",
@@ -89,15 +109,12 @@ analyze_survey <- function(mps, vars){
         stat_1 = "**Female**",
         ci_stat_1 = "**95% CI**",
         stat_2 = "**Male**",
-        ci_stat_2 = "**95% CI**")
+        ci_stat_2 = "**95% CI**"
+      )
+
+  } else {
+    stop("Neither 'sex' nor 'gender' was found in mps$variables.")
   }
 
-
-
-
-return(gtsummary_indicators)
-
-
+  return(gtsummary_indicators)
 }
-
-
